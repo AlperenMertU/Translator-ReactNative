e57f041150aefb18b78329c2cce306261f9cb916
Alperen Mert U <merturdogan@hotmail.com>
2023-05-28 00:01:30 +0300

    made a new main theme

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Output from './components/Output';
+import { StyleSheet, Text, View, TextInput, Image, } from 'react-native';
 import Input from './components/Input';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <Input/>
-     
+
+
+      <View style={styles.upperCase}>
+        <TextInput style={styles.input}/>
+      </View>
+
+
+
+      <View style={styles.wolfHead}>
+        <Image
+          style={styles.img}
+          source={require('./assets/wolfhead.png')}
+        />
+      </View>
+
+      <View style={styles.lowerCase}>
+      <TextInput style={styles.output}/>
+      </View>
+
+      <View style={styles.otherElementZ}>
+         <Text>Copy</Text>
+      </View>
+
     </View>
   );
 }
@@ -15,8 +35,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  upperCase: {
+    width: "100%",
+    height: 488,
+    backgroundColor: "#6590e0",
+  },
+  lowerCase: {
+    position: "absolute",
+    width: "100%",
+    height: 480,
+    backgroundColor: "white",
+    borderRadius: 20,
+    marginTop: 320,
+  },
+  wolfHead: {
+    zIndex: 99,
+    width: 200,
+    height: 100,
+    position: "absolute",
+  },
+  img: {
+    transform: [{ translateY: 230 }, { translateX: 68 }],
+    top: "50%",
+    left: "50%",
+    width: 70,
+    height: 70,
+  },
+  input:{
+    backgroundColor:"red",
+  },
+  output:{
+    backgroundColor:"yellow",
+  }
+
 });
