cf0fc61064b97527667fcc72a0e8951b259aa2a3
Alperen Mert U <merturdogan@hotmail.com>
2023-05-31 13:32:39 +0300

    new styling and bottom tabs process

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
 import Help from "../screens/Help";
 import Home from "../screens/Home";
@@ -13,9 +13,12 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator screenOptions={{
-      tabBarShowLabel: false,
-      tabBarStyle: {
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+    
+        tabBarStyle: {
           position: 'absolute',
           bottom: 25,
           left: 20,
@@ -23,29 +26,82 @@ function Tabs() {
           elevation: 0,
           backgroundColor: '#ffffff',
           borderRadius: 15,
-          height: 90,
-      }
-  }}>
-    <Tab.Screen name="Help" component={Help} />
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Info" component={Info} />
-  </Tab.Navigator>
+          height: 70,
+          ...styles.shadow
+        },
+      }}
+    >
 
+  
+
+
+      <Tab.Screen name="help" component={Help}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
+              <Image
+                source={require('../assets/home.png')}
+                resizeMode="contain"
+                style={{
+                  backgroundColor: 'red',
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'gray',
+                }}
+              />
+              <Text style={{ color: focused ? 'red' : 'white' }}>𐰔</Text>
+            </View>
+          ),
+        }}
+      />
+
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: "center", justifyContent: "center", top: 10 }}>
+            <Image source={require("../assets/home.png")}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "red" : "gray"
+              }}
+            />
+            <Text style={{ color: focused ? "red" : "white" }}>HOME</Text>
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name="Info" component={Info} options={{
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: "center", justifyContent: "center", top: 10 }}>
+            <Image source={require("../assets/help.png")}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "red" : "gray"
+              }}
+            />
+            <Text style={{ color: focused ? "red" : "white" }}>INFO</Text>
+          </View>
+        ),
+      }} />
+    </Tab.Navigator>
   );
 }
 
 
 
 const styles = StyleSheet.create({
-  container:{
-    position:"absolute",
-    bottom:25,
-    left:20,
-    right:20,
-    elevation:0,
-    backgroundColor:"red",
-    borderRadius:15,
-    height:90,
+  shadow: {
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
   },
 })
 
